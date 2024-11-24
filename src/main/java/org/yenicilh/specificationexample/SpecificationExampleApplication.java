39f555ccd9a155e8636caaaef5e43b8d6214d1cc
hyenicil <57847728+hyenicil@users.noreply.github.com>
2024-11-24 17:19:30 +0300

    Specification project yml file prepared.

---
+++ src/main/java/org/yenicilh/specificationexample/SpecificationExampleApplication.java
@@ -0,0 +1,13 @@
+package org.yenicilh.specificationexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecificationExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpecificationExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/yenicilh/specificationexample/SpecificationExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yenicilh.specificationexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpecificationExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
